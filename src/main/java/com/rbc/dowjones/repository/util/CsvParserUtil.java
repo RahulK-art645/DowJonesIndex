34a4bd4060477d5e3fa3185d4e5e0de13210be81
RahulK-art645 <rahul.koli@hcltech.com>
2026-01-24 06:59:35 -0800

    fIXES CSVUTIL CLASS

--- src/main/java/com/rbc/dowjones/repository/util/CsvParserUtil.java
+++ src/main/java/com/rbc/dowjones/repository/util/CsvParserUtil.java
@@ -2,6 +2,9 @@ package com.rbc.dowjones.repository.util;
 
 import com.rbc.dowjones.repository.exception.CsvProcessingException;
 import com.rbc.dowjones.repository.model.StockData;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
@@ -9,10 +12,8 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 
 @Component
 public class CsvParserUtil {
@@ -26,7 +27,7 @@ public class CsvParserUtil {
             "previous_weeks_volume","next_weeks_open","next_weeks_close",
             "percent_change_next_weeks_price","days_to_next_dividend","percent_return_next_dividend");
    private static final DateTimeFormatter ISO_DATE= DateTimeFormatter.ISO_LOCAL_DATE;
-   private static final DateTimeFormatter SLASH_DATE=DateTimeFormatter.ofPattern("M/d/yyy");
+   private static final DateTimeFormatter DATE_FORMAT=DateTimeFormatter.ofPattern("M/d/yyy");
 
    public List<StockData> parse(MultipartFile file){
 
@@ -34,93 +35,61 @@ public class CsvParserUtil {
 
         Set<String> csvKeys=new HashSet<>();
         try(BufferedReader reader = new BufferedReader(new
-                InputStreamReader(file.getInputStream()))){
+                InputStreamReader(file.getInputStream()));
 
-            String headerLine=reader.readLine();
-            if (headerLine == null){
-                throw new CsvProcessingException("CSV file is empty");
-            }
+            CSVParser csvParser=new CSVParser(reader, CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).setTrim(true).build())){
+                Map<String, Integer> headerMap=csvParser.getHeaderMap();
 
-            String[] headers=headerLine.split(",", -1);
+                if (headerMap.size() < EXPECTED_COLUMNS){
+                    throw new CsvProcessingException("Invalid Csv header. Expected 16 columns but found"+ headerMap.size());
 
-            if (headers.length != EXPECTED_COLUMNS){
-                throw  new CsvProcessingException("Invalid CSV header. Expected" + EXPECTED_COLUMNS +"columns");
-            }
-            for (int i=0; i<EXPECTED_HEADERS.size(); i++){
-                if (!headers[i].trim().equalsIgnoreCase(EXPECTED_HEADERS.get(i))){
-                    throw new CsvProcessingException("Invalid header at position " + i + " Expected: "+EXPECTED_HEADERS.get(i));
-                }
-            }
-
-            String line;
-            int linenumber=1;
-
-            /* ROW PROCESSING*/
-
-            while((line=reader.readLine()) != null){
-
-                linenumber++;
-                if (line.trim().isEmpty()){
-                    continue; // skip emty lines
-                }
-                String[] columns= line.split(",", -1);
-
-                if(columns.length != EXPECTED_COLUMNS){
-                    throw new CsvProcessingException("Invalid column count at line"+ linenumber+
-                            ". Expected "+ EXPECTED_COLUMNS +" but found "+columns.length);// skip Invalid/ incomplete
                 }
 
-                StockData data= new StockData();
-
-
-                data.setQuarter(parseNullableInt(columns[0]));
-                String stock=columns[1].trim();
-                if (stock.isEmpty()){
-                    throw new CsvProcessingException("Stock ticker is missing at line"+ linenumber);
-                }
-
-                if (!stock.matches("^[A-Z]+$")){
-                    throw new CsvProcessingException("Invalid stock ticker (only uppercase allowed), Number not allowed at line"+linenumber);
-                }
-                data.setStock(stock);
-
-
-                try {
-                    String dateStr=columns[2].trim();
-                    LocalDate date;
-
-                    if (dateStr.contains("_")){
-                        date=LocalDate.parse(dateStr, ISO_DATE);
-                    } else if (dateStr.contains("/")){
-                        date=LocalDate.parse(dateStr, SLASH_DATE);
-                    } else {
-                        throw new CsvProcessingException("Invalid date format at line: "+linenumber);
+                for(String expected : EXPECTED_HEADERS){
+                    if (!headerMap.containsKey(expected)){
+                        throw new CsvProcessingException("Missing required header" + expected);
                     }
-                    data.setDate(date);
-
-                }catch (Exception e){
-                    throw new CsvProcessingException("Invalid date at line"+linenumber);
                 }
-                data.setOpen(parsePositiveBigDecimal(columns[3],"open", linenumber));
-                data.setHigh(parsePositiveBigDecimal(columns[4],"high", linenumber));
-                data.setLow(parsePositiveBigDecimal(columns[5],"low", linenumber));
-                data.setClose(parsePositiveBigDecimal(columns[6], "close", linenumber));
-                data.setVolume(parsePositiveLong(columns[7],"volume",linenumber));
 
-                data.setPercentChangePrice(parseNullableBigDecimal(columns[8]));
-                data.setPercentChangeVolumeOverLastWk(parseNullableBigDecimal(columns[9]));
-                data.setPreviousWeeksVolume(parseNullableLong(columns[10]));
 
-                data.setNextWeeksOpen(parseNullableBigDecimal(columns[11]));
-                data.setNextWeeksClose(parseNullableBigDecimal(columns[12]));
-                data.setPercentChangeNextWeeksPrice(parseNullableBigDecimal(columns[13]));
+            int linenumber=1;
+            for(CSVRecord record : csvParser){
+                linenumber++;
 
-                data.setDaysToNextDividend(parseNullableInt(columns[14]));
-                data.setPercentReturnNextDividend(parseNullableBigDecimal(columns[15]));
+                if (record.size() < EXPECTED_COLUMNS){
+                    throw new CsvProcessingException("Invalid columns count at line"+linenumber);
+
+                }
+                StockData data =new StockData();
+
+                data.setQuarter(parseNullableInt(record.get("quarter"), linenumber));
+
+
+                data.setStock(parseStock(record.get("stock"), linenumber));
+                data.setDate(parseDate(record.get("date"), linenumber));
+
+
+
+                data.setOpen(parsePositiveBigDecimal(record.get("open"),"open", linenumber));
+                data.setHigh(parsePositiveBigDecimal(record.get("high"), "high",linenumber));
+                data.setLow(parsePositiveBigDecimal(record.get("low"),"low", linenumber));
+                data.setClose(parsePositiveBigDecimal(record.get("close"), "close",linenumber));
+                data.setVolume(parsePositiveLong(record.get("volume"),"volume",linenumber));
+
+                data.setPercentChangePrice(parseNullableBigDecimal(record.get("percent_change_price"),linenumber));
+                data.setPercentChangeVolumeOverLastWk(parseNullableBigDecimal(record.get("percent_change_volume_over_last_wk"), linenumber));
+                data.setPreviousWeeksVolume(parseNullableLong(record.get("previous_weeks_volume"), linenumber));
+
+                data.setNextWeeksOpen(parseNullableBigDecimal(record.get("next_weeks_open"), linenumber));
+                data.setNextWeeksClose(parseNullableBigDecimal(record.get("next_weeks_close"), linenumber));
+                data.setPercentChangeNextWeeksPrice(parseNullableBigDecimal(record.get("percent_change_next_weeks_price"), linenumber));
+
+                data.setDaysToNextDividend(parseNullableInt(record.get("days_to_next_dividend"), linenumber));
+                data.setPercentReturnNextDividend(parseNullableBigDecimal(record.get("percent_return_next_dividend"),linenumber));
 
                 String key=data.getStock()+"_"+data.getDate();
                 if (!csvKeys.add(key)){
-                    throw new CsvProcessingException("Duplicate record in csv stock" + data.getStock()+" on "+data.getDate() + " at line"+linenumber);
+                    throw new CsvProcessingException("Duplicate record in csv stock" + data.getStock()+" on date "+data.getDate() + " at line"+linenumber);
                 }
 
                 records.add(data);
@@ -139,88 +108,113 @@ public class CsvParserUtil {
         return records;
     }
 
-    // ---------- helper methods ----------
+    private String parseStock(String v, int line){
 
+       if (v==null || v.trim().isEmpty()){
+           throw new CsvProcessingException("Stock ticker missing at line"+line);
+       }
+       if (!v.matches("^[A-Z]+$")){
+           throw new CsvProcessingException("Invalid stock ticker at line"+line+ "(Only uppercase letter allowed)");
+       }
+return v.trim();
+    }
 
-
-    private BigDecimal parseNullableBigDecimal(String v) {
-       if(v==null) return null;
-
-       String value = v.replace("$","").trim();
-       if(value.isEmpty()) return null;
+    private LocalDate parseDate(String v, int line){
 
        try{
-           return new BigDecimal(value);
-       }catch(NumberFormatException e){
-           return null;
+           return LocalDate.parse(v.trim(),DATE_FORMAT);
+       }catch (Exception e){
+           throw new CsvProcessingException("Invalid date at line"+line);
        }
     }
 
-    private Long parseNullableLong(String v) {
-        if (v==null) return null;
+    // ---------- helper methods ----------
 
-        String value=v.trim();
-        if(value.isEmpty()) return null;
+    private BigDecimal parsePositiveBigDecimal(String v, String field, int line) {
 
-        try{
-            return Long.valueOf(value);
-        }catch (NumberFormatException e)
-        {
-            return null;
+        if (v == null || v.trim().isEmpty()) {
+            throw new CsvProcessingException(field + " is empty at line " + line);
         }
-    }
-
-    private Integer parseNullableInt(String v) {
-        if (v == null) return null;
-
-        String value = v.trim();
-
-        if(value.isEmpty()) return null;
-
-        try{
-
-            return Integer.valueOf(value);
-        }catch(NumberFormatException e){
-            return null;
-        }
-    }
-
-    private BigDecimal parsePositiveBigDecimal(String v, String field, int line){
-
-        if (v==null || v.trim().isEmpty()){
-            throw new CsvProcessingException(field + " is empty at line "+line);
-        }
-        try{
-            BigDecimal val=new BigDecimal(v.replace("$", "").trim());
-            if (val.compareTo(BigDecimal.ZERO)<=0){
-                throw new CsvProcessingException(field +"must be >0 at line"+ line);
+        try {
+            BigDecimal val = new BigDecimal(v.replace("$", "").trim());
+            if (val.compareTo(BigDecimal.ZERO) <= 0) {
+                throw new CsvProcessingException(field + "must be >0 at line" + line);
             }
             return val;
-        } catch (CsvProcessingException e){
+        } catch (CsvProcessingException e) {
             throw e;
-        } catch (Exception e){
-            throw new CsvProcessingException("Invalid "+ field +"at line "+ line);
+        } catch (Exception e) {
+            throw new CsvProcessingException("Invalid " + field + "at line " + line);
+        }
+    }
+
+        private Long parsePositiveLong(String v, String field,int line){
+
+            if (v == null || v.trim().isEmpty()) {
+                throw new CsvProcessingException(field + " is empty at line " + line);
+            }
+
+            try {
+                Long val = Long.valueOf(v.trim());
+                if (val <= 0) {
+                    throw new CsvProcessingException(field + "must be >0 at line" + line);
+                }
+                return val;
+
+            } catch (CsvProcessingException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new CsvProcessingException("Invalid " + field + " value at line" + line);
+            }
         }
 
 
+
+    private BigDecimal parseNullableBigDecimal(String v, int line) {
+       if(v==null || v.trim().isEmpty()) {
+           return null;
+       }
+
+       try{
+           return new BigDecimal(v.replace("$","").trim());
+       }catch(Exception e){
+           throw new CsvProcessingException("Invalid numeric value at line"+line);
+       }
     }
 
-    private Long parsePositiveLong(String v, String field, int line){
+    private Long parseNullableLong(String v, int line) {
+        if (v==null || v.trim().isEmpty()) {
+            return null;
+        }
+            try{
+                return Long.valueOf(v.trim());
+            } catch (Exception e){
+                throw new CsvProcessingException("Invalid long value at line"+line);
+            }
 
-        if (v==null || v.trim().isEmpty()){
-            throw new CsvProcessingException(field + " is empty at line "+line);
+        }
+
+
+    private Integer parseNullableInt(String v, int line) {
+        if (v == null || v.trim().isEmpty()){
+
+            return null;
         }
 
         try{
-        Long val=parseNullableLong(v);
-        if (val <=0){
-            throw new CsvProcessingException(field+"must be >0 at line"+line);
-        }
-        return val;
 
-    } catch (Exception e){
-         throw new CsvProcessingException("Invalid" + field +"at line"+line);
+            return Integer.valueOf(v.trim());
+        }catch(Exception e){
+
+           throw new CsvProcessingException("Invalid interger at line"+ line);
         }
     }
 
-}
+
+
+
+    }
+
+
+
+
