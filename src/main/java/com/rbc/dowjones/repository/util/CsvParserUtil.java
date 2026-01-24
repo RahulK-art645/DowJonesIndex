ecdb331f094c84901ba1206d48124adb9aaac081
RahulK-art645 <rahul.koli@hcltech.com>
2026-01-24 03:33:55 -0800

    Fixed

--- src/main/java/com/rbc/dowjones/repository/util/CsvParserUtil.java
+++ src/main/java/com/rbc/dowjones/repository/util/CsvParserUtil.java
@@ -22,7 +22,7 @@ public class CsvParserUtil {
             "quarter","stock",
             "date",
             "open","high","low","close", "volume",
-            "percent_change_price","percent_change_volume_over_lastwk",
+            "percent_change_price","percent_change_volume_over_last_wk",
             "previous_weeks_volume","next_weeks_open","next_weeks_close",
             "percent_change_next_weeks_price","days_to_next_dividend","percent_return_next_dividend");
    private static final DateTimeFormatter ISO_DATE= DateTimeFormatter.ISO_LOCAL_DATE;
