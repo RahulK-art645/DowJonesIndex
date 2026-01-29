c4b36070d320edc75cb6a254a411bfa9221cfac2
RahulK-art645 <rahul.koli@hcltech.com>
2026-01-29 02:16:43 -0800

    Updated erroros

--- src/test/java/com/rbc/dowjones/repository/StockDataRepositoryTest.java
+++ src/test/java/com/rbc/dowjones/repository/StockDataRepositoryTest.java
@@ -5,6 +5,7 @@ import com.rbc.dowjones.repository.repository.StockDataRepository;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class StockDataRepositoryTest {
 
     @Autowired
--- src/test/java/com/rbc/dowjones/repository/StockDataServicePerformanceTest.java
+++ src/test/java/com/rbc/dowjones/repository/StockDataServicePerformanceTest.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class StockDataServicePerformanceTest {
 
     @Autowired
