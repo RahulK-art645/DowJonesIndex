ef32192f7a4b0106b9f48ace11a4e9d6ae2622cf
RahulK-art645 <rahul.koli@hcltech.com>
2026-01-29 02:38:38 -0800

    Fixes

--- src/test/java/com/rbc/dowjones/repository/StockDataServicePerformanceTest.java
+++ src/test/java/com/rbc/dowjones/repository/StockDataServicePerformanceTest.java
@@ -2,6 +2,7 @@ package com.rbc.dowjones.repository;
 
 import com.rbc.dowjones.repository.dto.BulkUploadResponseDto;
 import com.rbc.dowjones.repository.service.StockDataService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("perf")
+@Disabled("Performance test - run manually")
 public class StockDataServicePerformanceTest {
 
     @Autowired
